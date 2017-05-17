a7e249000ef435818feee5546015a614a76d03ce
Ilia Konnov <iliakonnov@gmail.com>
2017-05-17 20:44:21 +0300

    Начал делать шестиугольник для бонуса

--- snake_game/Bonuses/AntiApple.cs
+++ snake_game/Bonuses/AntiApple.cs
@@ -8,6 +8,8 @@ namespace snake_game.Bonuses
 {
     class AntiAppleManager : IBonusManager
     {
+        readonly MainGame.Config.BonusConfigClass.AntiAppleConfigClass _config;
+        PolygonF _hex;
         public string Name => "antiapple";
 
         public void Draw(SpriteBatch sb)
@@ -15,9 +17,22 @@ namespace snake_game.Bonuses
             throw new NotImplementedException();
         }
 
+        public AntiAppleManager(MainGame.Config.BonusConfigClass.AntiAppleConfigClass config)
+        {
+            _config = config;
+        }
+
         public void LoadContent(GraphicsDevice graphicsDevice)
         {
-            throw new NotImplementedException();
+            var halfSqrt3 = Math.Sqrt(3) / 2;
+            _hex = new PolygonF(new Vector2[] {
+                new Vector2(1 * _config.Size, 0),
+                new Vector2((float)(0.5 * _config.Size), (float)(halfSqrt3 * _config.Size)),
+                new Vector2((float)(-0.5  * _config.Size), (float)(halfSqrt3 * _config.Size)),
+                new Vector2(-1 * _config.Size, 0),
+                new Vector2((float)(-0.5 * _config.Size), (float)(-halfSqrt3 * _config.Size)),
+                new Vector2((float)(0.5 * _config.Size), (float)(-halfSqrt3 * _config.Size))
+            });
         }
 
         public void Update(GameTime gameTime, int fullTime, IBonusManager[] bonuses, CircleF[] snakePoints, Rectangle size)
